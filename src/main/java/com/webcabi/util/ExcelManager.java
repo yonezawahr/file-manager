61a77f7b0b01c94410514b88b8b47d248ee5257a
ONE <yonezawahr@gmail.com>
2016-02-16 00:19:43 +0900

    Change indent from tab to space

--- src/main/java/com/webcabi/util/ExcelManager.java
+++ src/main/java/com/webcabi/util/ExcelManager.java
@@ -4,32 +4,29 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelManager {
-	private Workbook workbook;
-	
-	public void open(File file) {
-		try {
-			FileInputStream input = new FileInputStream(file);
-			this.workbook = new XSSFWorkbook(input);
-		} catch(FileNotFoundException ex) {
-			ex.printStackTrace();
-		} catch(IOException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	public void close() {
-		try {
-			this.workbook.close();
-		} catch(IOException ex) {
-			ex.printStackTrace();
-		}
-	}
+    private Workbook workbook;
+
+    public final void open(final File file) {
+        try {
+            FileInputStream input = new FileInputStream(file);
+            this.workbook = new XSSFWorkbook(input);
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public final void close() {
+        try {
+            this.workbook.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
